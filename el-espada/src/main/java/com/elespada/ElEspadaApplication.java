98e27dc60d74ec07776e6327b86d895710681881
Zach Parker <zkparker@yahoo.com>
2020-03-05 21:39:35 -0600

    Initial commit

---
+++ el-espada/src/main/java/com/elespada/ElEspadaApplication.java
@@ -0,0 +1,13 @@
+package com.elespada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElEspadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElEspadaApplication.class, args);
+	}
+
+}
